2ebe461971c5801e147294b8a514af358c04e4ab
HeSer <B@nd@r1359>
2023-01-03 14:28:49 -0500

    update Final

---
+++ index.js
@@ -0,0 +1,22 @@
+// this is the callnege 
+
+
+// initiate the count 
+
+
+
+// increase the count 
+
+
+// save the count 
+
+
+// show in html page 
+let count = 1
+
+function increment() {
+
+    count += 1
+    document.getElementById("count-el").innerText = count
+
+};
